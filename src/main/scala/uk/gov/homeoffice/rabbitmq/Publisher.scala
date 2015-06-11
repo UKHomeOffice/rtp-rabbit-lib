39ffff3dd60827c2b20bca1ad59bbffc8a18720b
IrfanAnsari <irfanaansari@gmail.com>
2015-06-11 14:29:19 +0100

    Added some print statement to debug issue with validation service unable to send message to rabbit MQ

--- src/main/scala/uk/gov/homeoffice/rabbitmq/Publisher.scala
+++ src/main/scala/uk/gov/homeoffice/rabbitmq/Publisher.scala
@@ -12,27 +12,35 @@ trait Publisher {
   this: Queue with Rabbit =>
   
   def publish(json: JValue): Future[JValue Or JsonError] = {
+    println(s"Going to publish json : ${json}")
+
     val promise = Promise[JValue Or JsonError]()
 
     Future {
       try {
         val channel = connection.createChannel()
         channel.confirmSelect()
-
+        println(s"Rabbit mq channel $channel selected")
         channel.addConfirmListener(new ConfirmListener {
           def handleAck(deliveryTag: Long, multiple: Boolean) = {
+            println(s"Handle acknowledgment for $deliveryTag")
             promise success Good(json)
           }
 
           def handleNack(deliveryTag: Long, multiple: Boolean) = {
+            println(s"Handle non acknowledgment for $deliveryTag")
             promise success Bad(JsonError(json, s"Rabbit NACK - Failed to publish ${pretty(render(json))}"))
           }
         })
 
         println(s"Publishing to $connection") // TODO - Log as debug
         channel.basicPublish("", queue(channel), MessageProperties.PERSISTENT_BASIC, compact(render(json)).getBytes)
+        print(s"Message has been published to the channel")
       } catch {
-        case t: Throwable => promise failure new RabbitPublisherException(t, json)
+        
+        case t: Throwable => {
+          println(s"Unable to send message due to ERROR : ${t.printStackTrace()}")
+          promise failure new RabbitPublisherException(t, json)}
       }
     }
 
