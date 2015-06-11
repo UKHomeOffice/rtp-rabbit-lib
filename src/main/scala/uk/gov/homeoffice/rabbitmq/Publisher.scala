cc8780e5d25fc5aa52db4a5f577e0f3560256830
David Ainslie <dainslie@gmail.com>
2015-06-11 14:55:17 +0100

    Reverted to original error publishing

--- src/main/scala/uk/gov/homeoffice/rabbitmq/Publisher.scala
+++ src/main/scala/uk/gov/homeoffice/rabbitmq/Publisher.scala
@@ -6,23 +6,36 @@ import org.json4s.JValue
 import org.json4s.JsonAST.{JObject, JString}
 import org.json4s.native.JsonMethods._
 import org.scalactic.{Bad, Good, Or}
-import com.rabbitmq.client.{Channel, ConfirmListener, MessageProperties}
+import com.rabbitmq.client.{ConfirmListener, MessageProperties}
 import uk.gov.homeoffice.json.JsonError
 
 trait Publisher {
   this: Queue with Rabbit =>
-  
+
   def publish(json: JValue): Future[JValue Or JsonError] = {
     val promise = Promise[JValue Or JsonError]()
 
-    def ack = promise success Good(json)
+    Future {
+      try {
+        val channel = connection.createChannel()
+        channel.confirmSelect()
 
-    def nack = promise success Bad(JsonError(json, s"Rabbit NACK - Failed to publish JSON ${pretty(render(json))}"))
+        channel.addConfirmListener(new ConfirmListener {
+          def handleAck(deliveryTag: Long, multiple: Boolean) = {
+            promise success Good(json)
+          }
 
-    def error(t: Throwable) = promise failure new RabbitException(JsonError(json, "Failed to publish JSON", Some(t)))
+          def handleNack(deliveryTag: Long, multiple: Boolean) = {
+            promise success Bad(JsonError(json, s"Rabbit NACK - Failed to publish ${pretty(render(json))}"))
+          }
+        })
 
-    println(s"Publishing JSON to $connection") // TODO - Log as debug
-    publish(json, queue, ack, nack, error)
+        println(s"Publishing to $connection") // TODO - Log as debug
+        channel.basicPublish("", queue(channel), MessageProperties.PERSISTENT_BASIC, compact(render(json)).getBytes)
+      } catch {
+        case t: Throwable => promise failure new RabbitException(JsonError(json, "Failed to publish JSON", Some(t)))
+      }
+    }
 
     promise.future
   }
@@ -30,32 +43,28 @@ trait Publisher {
   def publish(e: JsonError): Future[JsonError] = {
     val promise = Promise[JsonError]()
 
-    def ack = promise success e
+    Future {
+      try {
+        val channel = connection.createChannel()
+        channel.confirmSelect()
 
-    def nack = promise success e.copy(error = s"Rabbit NACK - Failed to publish error JSON: ${e.error}")
+        channel.addConfirmListener(new ConfirmListener {
+          def handleAck(deliveryTag: Long, multiple: Boolean) = promise success e
 
-    def error(t: Throwable) = promise failure new RabbitException(e.copy(error = s"Failed to publish error JSON: ${e.error}"))
+          def handleNack(deliveryTag: Long, multiple: Boolean) = {
+            promise success e.copy(error = s"Rabbit NACK - Failed to publish error JSON: ${e.error}")
+          }
+        })
 
-    println(s"Publishing error JSON to $connection") // TODO - Log as debug
-    publish(e.json merge JObject("error" -> JString(e.error)), errorQueue, ack, nack, error)
+        println(s"Publishing error to $connection") // TODO - Log as debug
+        channel.basicPublish("", errorQueue(channel), MessageProperties.PERSISTENT_BASIC, compact(render(e.json merge JObject("error" -> JString(e.error)))).getBytes)
+      } catch {
+        case t: Throwable =>
+          t.printStackTrace()
+          promise failure new RabbitException(e.copy(error = s"Failed to publish error JSON: ${e.error}"))
+      }
+    }
 
     promise.future
-  }
-
-  private[rabbitmq] def publish(json: JValue, queue: Channel => String, ack: => Any, nack: => Any, error: Throwable => Any) = Future {
-    try {
-      val channel = connection.createChannel()
-      channel.confirmSelect()
-
-      channel.addConfirmListener(new ConfirmListener {
-        def handleAck(deliveryTag: Long, multiple: Boolean) = ack
-
-        def handleNack(deliveryTag: Long, multiple: Boolean) = nack
-      })
-
-      channel.basicPublish("", queue(channel), MessageProperties.PERSISTENT_BASIC, compact(render(json)).getBytes)
-    } catch {
-      case t: Throwable => error(t)
-    }
   }
 }
---
+++ src/test/scala/uk/gov/homeoffice/rabbitmq/WithConsumerSpec.scala
@@ -0,0 +1,23 @@
+package uk.gov.homeoffice.rabbitmq
+
+import org.json4s.JsonAST.JObject
+import org.specs2.mutable.Specification
+import uk.gov.homeoffice.json.JsonError
+
+class WithConsumerSpec extends Specification with RabbitSpec {
+  "Message" should {
+    "be consumed from error queue" in {
+      val publisher = new Publisher with WithConsumer with WithQueue with WithRabbit {
+        def consume(body: Array[Byte]) = println("Consumed")
+
+        override def consumeError(body: Array[Byte]) = println("Consumed Error")
+      }
+
+      publisher.publish(JsonError(JObject(), "Error"))
+      //publisher.publish(JObject())
+
+      Thread.sleep(2000)
+      ok
+    }
+  }
+}
