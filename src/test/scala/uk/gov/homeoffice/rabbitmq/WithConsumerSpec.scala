3b5884cc51dcda864bf8975db76639a7717db305
David Ainslie <dainslie@gmail.com>
2015-06-12 10:22:25 +0100

    Can Jenkins now run this spec?

--- src/test/scala/uk/gov/homeoffice/rabbitmq/WithConsumerSpec.scala
+++ src/test/scala/uk/gov/homeoffice/rabbitmq/WithConsumerSpec.scala
@@ -5,8 +5,7 @@ import org.specs2.mutable.Specification
 import uk.gov.homeoffice.json.JsonError
 
 class WithConsumerSpec extends Specification with RabbitSpec {
-  // TODO DOES NOT RUN ON JENKINS ???
-  /*"Message" should {
+  "Message" should {
     "be consumed from error queue" in {
       val publisher = new Publisher with WithConsumer with WithQueue with WithRabbit {
         def consume(body: Array[Byte]) = println("Consumed")
@@ -20,5 +19,5 @@ class WithConsumerSpec extends Specification with RabbitSpec {
       Thread.sleep(2000)
       ok
     }
-  }*/
+  }
 }
