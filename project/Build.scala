4e70145ac07984af81172d1ab3f2b3f8e936e994
Ryan Da Costa <ryandac@gmail.com>
2015-06-15 10:09:18 +0100

    pull out images and fonts into core caseworker

--- project/Build.scala
+++ project/Build.scala
@@ -4,7 +4,7 @@ import sbt._
 object Build extends Build {
   val moduleName = "rtp-rabbit-lib"
 
-  lazy val root = Project(id = moduleName, base = file("."))
+  lazy val rabbit = Project(id = moduleName, base = file("."))
     .configs(IntegrationTest)
     .settings(Defaults.itSettings: _*)
     .settings(
@@ -31,10 +31,35 @@ object Build extends Build {
         "com.typesafe" % "config" % "1.2.1" withSources(),
         "com.typesafe.akka" %% "akka-actor" % "2.3.9" withSources(),
         "com.rabbitmq" % "amqp-client" % "3.5.3" withSources(),
-        "org.scalactic" %% "scalactic" % "2.2.4" withSources(),
-        "uk.gov.homeoffice" %% "rtp-io-lib" % "1.0-SNAPSHOT" withSources()),
+        "org.scalactic" %% "scalactic" % "2.2.4" withSources()
+      ),
       libraryDependencies ++= Seq(
         "org.specs2" %% "specs2-core" % "3.6" % "test, it" withSources(),
         "org.specs2" %% "specs2-mock" % "3.6" % "test, it" withSources(),
         "org.specs2" %% "specs2-matcher-extra" % "3.6" % "test, it" withSources()))
+
+  def existsLocallyAndNotOnJenkins(filePath: String) = {
+    new java.io.File(filePath).exists && !new java.io.File(filePath + "/nextBuildNumber").exists()
+  }
+  def checkFileExistsInADirectoryBelow(filePath: String, times: Int = 0): String = {
+    if (times > 3 || existsLocallyAndNotOnJenkins(filePath)) filePath
+    else checkFileExistsInADirectoryBelow("../"+filePath, times+1)
+  }
+  val rtpiolibPath = checkFileExistsInADirectoryBelow("../rtp-io-lib")
+  lazy val root = if (existsLocallyAndNotOnJenkins(rtpiolibPath)) {
+    println("=====================")
+    println("Build Locally rabbit ")
+    println("=====================")
+    val actualRtpIoPath = "../rtp-io-lib" //NEEDS TO BE THIS PATH
+    val rtpiolib =ProjectRef(file(actualRtpIoPath), "rtp-io-lib")
+    rabbit.dependsOn(rtpiolib)
+  } else {
+    println("=====================")
+    println("Build Jenkins rabbit ")
+    println("=====================")
+    rabbit.settings(
+      libraryDependencies ++= Seq(
+        "uk.gov.homeoffice" %% "rtp-io-lib" % "1.0-SNAPSHOT" withSources()
+      ))
+  }
 }
