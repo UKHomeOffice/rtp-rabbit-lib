96a40cb1a6b44cfd32a3c4a3e3439e06163a017e
David Ainslie <dainslie@gmail.com>
2015-07-21 14:19:52 +0100

    Scaladoc

--- src/main/scala/uk/gov/homeoffice/rabbitmq/RetryStrategy.scala
+++ src/main/scala/uk/gov/homeoffice/rabbitmq/RetryStrategy.scala
@@ -2,6 +2,14 @@ package uk.gov.homeoffice.rabbitmq
 
 import scala.concurrent.duration._
 
+/**
+ * A mutable class to track retries against failed consumption of Rabbit messages, where the exception has been deemed to retry the message consumption.
+ * Note that this class is mutable and not thread safe, being initially intended to be only used by [[uk.gov.homeoffice.rabbitmq.ConsumerActor]]
+ * @param delay Duration until next retry
+ * @param numberOfRetries Int current number of retries
+ * @param maximumNumberOfRetries Int the maximum number of retries
+ * @param incrementStrategy Function[Duration => Duration] which dictates how the delay Duration is calculated.
+ */
 class RetryStrategy(var delay: Duration = 10 seconds, var numberOfRetries: Int = 0,
                     maximumNumberOfRetries: Int = 10,
                     incrementStrategy: Duration => Duration = d => d * 2) {
